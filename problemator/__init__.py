b8df04569e54e761b78a31afd68464e9da892092
DedInc <visitanimation@gmail.com>
2022-11-18 10:39:18 +0600

    v1.0.5

--- problemator/__init__.py
+++ problemator/__init__.py
@@ -1,1 +1,1 @@
-from .problemator import loadSession, getCategories, getCategoryByID, getCategoryByName, generateProblems, checkProblem
+from .problemator import loadSession, getCategories, getCategory, generateProblem, checkProblem
--- problemator/problemator.py
+++ problemator/problemator.py
@@ -1,7 +1,9 @@
 from requests import Session, get
+from requests.cookies import create_cookie
 from user_agent import generate_user_agent
 
 categories = {}
+
 
 def searchCategories(cats):
     global categories
@@ -9,25 +11,17 @@ def searchCategories(cats):
         if 'Subcategories' in c:
             searchCategories(c['Subcategories'])
         else:
-            categories[c['LinkTo']] = {'name': c['Display'], 'id': len(categories)}
+            categories[c['LinkTo']] = len(categories)
 
 
 def getCategories():
     return categories
 
 
-def getCategoryByID(id):
+def getCategory(id):
     for cat in categories.keys():
-        if id == categories[cat]['id']:
+        if categories[cat] == id:
             return cat
-    return None
-
-
-def getCategoryByName(name):
-    for cat in categories.keys():
-        if name == categories[cat]['name']:
-            return cat
-    return None
 
 
 def loadSession():
@@ -42,18 +36,25 @@ def loadSession():
     searchCategories(r['Categories']['Categories'])
     API = r['domain']
 
-
 def checkProblem(problem, answer):
     lvl = problem['difficulty']
     pid = problem['id']
     machine = problem['machine']
+    for c in problem['session']:
+        cookie = create_cookie(name=c['name'], value=c['value'], domain=c['domain'])
+        s.cookies.set_cookie(cookie)
     r = s.get(f'{API}/input/wpg/checkanswer.jsp?attempt=1&difficulty={lvl}&load=true&problemID={pid}&query={answer}&s={machine}&type=InputField').json()
     return {'correct': r['correct'], 'hint': r['hint'], 'solution': r['solution']}
 
 
-def generateProblems(lvl=0, type='IntegerAddition', count=1):
+def generateProblem(lvl=0, type='IntegerAddition'):
     lvl = {0: 'Beginner', 1: 'Intermediate', 2: 'Advanced'}[lvl]
-    r = s.get(f'{API}/input/wpg/problem.jsp?count={count}&difficulty={lvl}&load=1&type={type}').json()
+    r = s.get(f'{API}/input/wpg/problem.jsp?count=1&difficulty={lvl}&load=1&type={type}').json()
     problems = r['problems']
     machine = r['machine']
-    return [{'text': problem['string_question'], 'image': problem['problem_image'], 'difficulty': lvl, 'id': problem['problem_id'], 'machine': machine} for problem in problems]
+    cookies = []
+    for c in s.cookies:
+        if c.name == 'JSESSIONID':
+            cookies.append({'name': c.name, 'value': c.value, 'domain': c.domain})
+    problem = problems[0]
+    return {'text': problem['string_question'], 'image': problem['problem_image'], 'difficulty': lvl, 'id': problem['problem_id'], 'machine': machine, 'session': cookies}
